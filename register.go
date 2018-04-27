6d9e62aa06dec9884fa07d8562955d289021c45c
okzk <okazaki@okzk.com>
2018-04-27 15:15:19 +0900

    support some broken certs in yubikey

--- register.go
+++ register.go
@@ -11,6 +11,7 @@ import (
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/asn1"
+	"encoding/hex"
 	"errors"
 	"time"
 )
@@ -124,6 +125,7 @@ func parseRegistration(buf []byte) (*Registration, []byte, error) {
 	}
 
 	buf = buf[:len(buf)-len(sig)]
+	fixCertIfNeed(buf)
 	cert, err := x509.ParseCertificate(buf)
 	if err != nil {
 		return nil, nil, err
@@ -184,6 +186,24 @@ func getRegisteredKey(appID string, r Registration) RegisteredKey {
 		Version:   u2fVersion,
 		KeyHandle: encodeBase64(r.KeyHandle),
 		AppID:     appID,
+	}
+}
+
+// fixCertIfNeed fixes broken certificates described in
+// https://github.com/Yubico/php-u2flib-server/blob/master/src/u2flib_server/U2F.php#L84
+func fixCertIfNeed(cert []byte) {
+	h := sha256.Sum256(cert)
+	switch hex.EncodeToString(h[:]) {
+	case
+		"349bca1031f8c82c4ceca38b9cebf1a69df9fb3b94eed99eb3fb9aa3822d26e8",
+		"dd574527df608e47ae45fbba75a2afdd5c20fd94a02419381813cd55a2a3398f",
+		"1d8764f0f7cd1352df6150045c8f638e517270e8b5dda1c63ade9c2280240cae",
+		"d0edc9a91a1677435a953390865d208c55b3183c6759c9b5a7ff494c322558eb",
+		"6073c436dcd064a48127ddbf6032ac1a66fd59a0c24434f070d4e564c124c897",
+		"ca993121846c464d666096d35f13bf44c1b05af205f9b4a1e00cf6cc10c5e511":
+
+		// clear the offending byte.
+		cert[len(cert)-257] = 0
 	}
 }
 
